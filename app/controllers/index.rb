16c8351206f66d8cb63a11509976d2eaa1c7b617
Apprentice <apprentice@devbootcamp.com>
2013-03-18 16:01:56 -0700

    added form functionality

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,5 +3,9 @@ get '/' do
 end
 
 post '/' do
-  
+  puts params[:form1][:name]
+  @player1 = Player.find_or_create_by_name(params[:form1][:name].downcase.strip)
+  @player2 = Player.find_or_create_by_name(params[:form2][:name].downcase.strip)
+  @game = Game.create(:player1_id => @player1.id, :player2_id => @player2.id)
+  erb :index
 end
